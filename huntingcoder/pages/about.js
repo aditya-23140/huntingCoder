b9940e01c7cca01be129b50566d9388ca8afee6d
Aditya Kumar <ironaditya12345@gmail.com>
2024-10-22 19:15:42 +0530

    Tutorial 28: Making about page

--- huntingcoder/pages/about.js
+++ huntingcoder/pages/about.js
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import styles from '@/styles/about.module.css'
 const About = () => {
   return (
-    <>
-      
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit ea officiis vel delectus, ab laudantium quae rerum praesentium iusto at accusamus ex corrupti, quam amet iure assumenda repellendus maiores architecto facilis vero ipsam sint aperiam corporis a? Rem illum molestiae in, sequi iure odio laborum maiores? Velit, accusantium? Dicta, odio excepturi quae esse quidem ut explicabo itaque nemo quibusdam fugit iure accusamus. Ex, consequatur tempora ratione pariatur, minus optio nisi dicta beatae a totam laborum fugit soluta asperiores iure error molestias enim in voluptatem saepe dolore dignissimos veritatis aliquam, recusandae quod. Commodi officia totam excepturi, fuga quam voluptatem quia labore sapiente eius pariatur eligendi voluptas accusantium. Error, autem sed numquam, reiciendis harum corrupti repellat sequi qui commodi quod soluta labore quasi ipsum neque, aliquid ea in atque. Culpa placeat exercitationem eum sequi expedita provident voluptates? Nulla, porro assumenda totam reprehenderit distinctio optio amet beatae est repellat fugit officia accusamus, tenetur delectus. Delectus inventore eum expedita sequi architecto sunt non natus optio voluptates atque quia sint, reprehenderit aut veniam odit molestiae est incidunt placeat hic temporibus tenetur assumenda eligendi consectetur! Blanditiis, esse consectetur? Sed, soluta eveniet. Recusandae perspiciatis soluta commodi quae! </p>
+      <h2>Services Offered</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, incidunt deserunt! Dolorem quas officiis cum laboriosam, quibusdam quisquam unde alias sapiente eligendi. Itaque nobis, modi sequi blanditiis harum minus debitis nesciunt quos quis nostrum?</p>
+      <p>We offer the following services</p>
+      <ul>
+        <li>Web Development</li>
+        <li>Mobile App Development</li>
+        <li>SEO</li>
+        <li>Content Writing</li>
+        <li>Graphic Design</li>
+      </ul>
+      <h2>Contact us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum optio reiciendis, repellat itaque nesciunt dolorem deleniti nam dolore, veniam, inventore obcaecati explicabo ex sequi nisi quidem consectetur sed? Qui neque possimus, temporibus dolorum laborum vitae non nulla quos totam, nam molestiae accusantium praesentium! Laudantium ipsa ducimus illo accusamus. Animi tenetur tempora magni accusamus iste.</p>
+    </div>
   )
 }
 
